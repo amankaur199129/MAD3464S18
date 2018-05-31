2d11cd0977ce1da360f44cc60f93a39a8d40248d
amankaur199129 <39779501+amankaur199129@users.noreply.github.com>
2018-05-31 17:26:35 -0400

    Add files via upload

---
+++ Activity.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author macstudent
+ */
+public class Activity {
+    public static void main(String[]arg)
+    {
+        for(int i=1;i<=5;i++){
+            for(int j= 1; j<=5;j++){
+                if(i==1 || i==5|| j==1|| j==5)
+                {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }            
+                    
+            }
+            System.out.println();
+        }
+    }
+    
+}
